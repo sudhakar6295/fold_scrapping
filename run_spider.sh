d98f5577dd3b4e632153275fd9b3f2205109ad88
sudhakar6295 <53999168+sudhakar6295@users.noreply.github.com>
2024-06-09 07:47:29 +0530

    Create run_spider.sh

---
+++ run_spider.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Activate virtual environment
+source /home/fold/python3.8/bin/activate
+
+# Navigate to your Scrapy project directory
+cd /home/fold/fold_scrapping
+
+# Run Scrapy spider
+scrapy crawl fold
+
+
